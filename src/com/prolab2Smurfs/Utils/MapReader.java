26487a649e40130ca8a4cdb7ea87c2659a56a5b2
ugurcanatas <ugurcanemre@hotmail.com>
2021-03-18 12:53:04 +0300

    🦄 - File Read updates

--- src/com/prolab2Smurfs/Utils/MapReader.java
+++ src/com/prolab2Smurfs/Utils/MapReader.java
@@ -4,6 +4,8 @@ import com.prolab2Smurfs.Dijkstra.SingleNode;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 import static com.prolab2Smurfs.Utils.Constants.*;
@@ -12,7 +14,7 @@ public class MapReader {
     private String mapString = "";
     private ArrayList<String> mapList = new ArrayList<>();
 
-    private SingleNode[][] nodes = new SingleNode[13][13];
+    private SingleNode[][] nodes = new SingleNode[13][11];
 
     public MapReader() {
     }
@@ -33,32 +35,48 @@ public class MapReader {
         File file = new File(mapPath);
         try {
             Scanner reader = new Scanner(file);
-            int i = 0;
+
+            ArrayList<String[]> grid = new ArrayList<>();
             while (reader.hasNextLine()) {
                 String data = reader.nextLine();
-                //System.out.println("Line " + i + " - " + data);
-                String rowF = data.split("\t")[0];
-                if (rowF.equals("1") || rowF.equals("0")) {
-                    String [] row = data.split("\t");
-                    for (int j = 0; j < row.length; j++) {
-                        if (row[j].equals("1")) {
-                            nodes[i][j] = new SingleNode(TYPE_PATH,i,j);
-                        }else {
-                            //WALL
-                            nodes[i][j] = new SingleNode(TYPE_WALL,i,j);
-                        }
+                String[] arr = data.split("\t");
+                System.out.println("READING" + Arrays.toString(arr));
+                //testing
+                if (arr.length > 4) {
+                    grid.add(arr);
+                }//else part is characters
+            }
+            System.out.println("ROWS: " + grid.size());
+            System.out.println("COLS: " + grid.get(0).length);
+
+            //< 13
+            for (int i = 0; i < grid.get(0).length; i++) {
+                // < 11
+                for (int j = 0; j < grid.size(); j++) {
+                    String type = grid.get(j)[i];
+                    if (type.equals("1")) {
+                        nodes[i][j] = new SingleNode(TYPE_PATH,i,j);
+                    } else {
+                        nodes[i][j] = new SingleNode(TYPE_WALL,i,j);
                     }
-                    i++;
                 }
             }
+
+            System.out.println("");
             for (int k = 0; k < 13; k++) {
-                nodes[11][k] = new SingleNode(TYPE_WALL,k,11);
-                nodes[12][k] = new SingleNode(TYPE_WALL,k,12);
+                for (int z = 0; z < 11; z++) {
+                    System.out.print(nodes[k][z].getType() + " \t");
+                }
+                System.out.println("");
             }
         } catch (
                 FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    public int getRandomCharacter () {
+        return new Random().nextInt(2);
     }
 
 
